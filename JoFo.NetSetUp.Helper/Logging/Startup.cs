5b0c1c8e719af4d77fa7f7dd692983bdb2a4c6ac
JoFo <github@johannesfortmann.de>
2023-05-29 08:46:53 +0200

    Add a basic setup for Serilog
    This helps tu setup Serilog really fast in all .Net projects.
    
    Example:
    
        Host.CreateDefaultBuilder().AddSeriLog(_configuration);

---
+++ JoFo.NetSetUp.Helper/Logging/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace JoFo.NetSetUp.Helper.Logging;
+
+public static class Startup
+{
+    public static void AddSeriLog(this IHostBuilder builder, IConfiguration configuration)
+    {
+        builder.UseSerilog((_, config) =>
+        {
+            config.ReadFrom.Configuration(configuration);
+            config.WriteTo.Console();
+        });
+    }
+}
